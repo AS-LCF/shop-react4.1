090948be7d4c1c9e4a34de0a8cdc85eb35bc3165
AS-LCF <483143aa@gmail.com>
2025-02-28 20:57:03 +0400

    add pages in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import './App.scss'
-//import Header from './components/Header'
-//import Footer from './components/Footer'
+
+import { Route, Router, Routes } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { useState } from 'react'
+import Home from './pages/Home'
+import ProductDetail from './pages/ProductDetail'
+import CartPage from './components/CartPage'
+
 
 export default function App(){
-  <>
-  </>
+  const [cartItems,setCartItems]=useState([])
+
+  const addToCart=(product) =>{
+    setCartItems(prev=>[...prev, product])
+  }
+
+  const remuveFromCart=(id)=>{
+    setCartItems(prev=>prev.filter(item => item.id !== id))
+  }
+
+
   return ( 
-    //<Header />
-    //<Footer />
+    <Router>
+      <Header/>
+      <main className='min-h-screen'>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+
+          <Route path="/product/:id" element={<ProductDetail 
+          addToCart={addToCart} />} />
+            
+            <Route path="/cart" element={<CartPage cartItems={cartItems}
+          removeFromCart={remuveFromCart} />} />
+        </Routes> 
+      </main>
+      <Footer/>
+    </Router>
 
   )
 }
